02acfb18ce3d9668b4f521ca1405c5f2408ac224
Katia Zherdetskaya <nekhaeva.e@yandex.ru>
2022-11-04 20:35:22 +0300

    04.11.22

--- scripts/index.js
+++ scripts/index.js
@@ -2,53 +2,33 @@ let buttonElement = document.querySelector('.profile__edit-button');
 let popupElement = document.querySelector('.popup');
 let closeElement = popupElement.querySelector('.popup__close');
 let saveChanges = popupElement.querySelector('.popup__submit');
+let formElement = document.querySelector('.popup__form');
+let closeButton = document.querySelector('.popup__submit');
+let nameProfile = document.querySelector('.profile__user-name');
+let jobProfile = document.querySelector('.profile__user-info');
+let nameInput = document.querySelector('.popup__item_name');
+let jobInput = document.querySelector('.popup__item_job');
 
 function openPopup() {
   popupElement.classList.add('popup_opened');
 };
 
-openPopup();
-
 function closePopup() {
   popupElement.classList.remove('popup_opened');
 };
 
-closePopup();
-
-function save() {
-  popupElement.classList.remove('popup_opened');
-};
-
-save();
-
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__container');
-let closeButton = document.querySelector('.popup__submit');
-// Находим поля формы в DOM
-let nameProfile = document.querySelector('.profile__user-name');
-let jobProfile = document.querySelector('.profile__user-info');
-let nameInput = document.querySelector('.popup__user-name');
-let jobInput = document.querySelector('.popup__user-info');
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  nameInput.value; // Получите значение полея nameInput из свойства value
-  jobInput.value; // Получите значение полея jobInput из свойства value
   nameProfile.textContent = nameInput.value; // Выберите элементы, куда должны быть вставлены значения полей и вставьте новые значения с помощью textContent
   jobProfile.textContent = jobInput.value; // Выберите элементы, куда должны быть вставлены значения полей и вставьте новые значения с помощью textContent
-  save();
+  closePopup();
 };
 
-buttonElement.addEventListener('.click', function () {
+buttonElement.addEventListener('click', function save() {
   openPopup();
   nameInput.value = nameProfile.textContent;
   jobInput.value = jobProfile.textContent;
 });
-
-buttonElement.addEventListener('click', openPopup);
 
 closeElement.addEventListener('click', closePopup);
 
