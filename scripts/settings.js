3b24961e56de032f45d68f99c73eaad6dd6b6d55
Ekaterina Zherdetskaya <112477055+KatyZhe@users.noreply.github.com>
2022-12-21 14:39:02 +0300

    21.12.22

---
+++ scripts/settings.js
@@ -0,0 +1,10 @@
+const validationSettings = {
+    formSelector: '.popup__form',
+    inputSelector: '.popup__item',
+    submitButtonSelector: '.popup__button',
+    inactiveButtonClass: 'popup__button_disabled',
+    inputErrorClass: 'popup__input_type_error',
+    errorClass: 'popup__span_active'
+};
+
+export default validationSettings;
